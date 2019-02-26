ce22349160903a256a314999a11325856de7f94b
Wryth <lucasbriatte@gmail.com>
2019-02-26 19:43:43 +0100

    Fragment 4, et 5, style, colors

--- app/src/main/java/fr/mydango/colormemory/Views/Activities/MainActivity.java
+++ app/src/main/java/fr/mydango/colormemory/Views/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package fr.mydango.colormemory;
+package fr.mydango.colormemory.Views.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import fr.mydango.colormemory.R;
 
 public class MainActivity extends AppCompatActivity {
 
